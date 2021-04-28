bf28cd595c62119e93e4e1a77053ac12be731175
marcstreeter <mstreeter@gmail.com>
2021-04-28 11:57:04 -0500

    lecture 56 done

--- myscript.js
+++ myscript.js
@@ -1,19 +1,14 @@
+var x = 0;
+var i = 0;
+while (x < 27) {
+    console.log(`step ${i} x is currently: ${x}`);
 
-var ham = 0;
-var cheese = 0;
-
-var report = "blank";
-
-if (ham >= 10 && cheese >= 10) {
-report = "Strong sales of both ham and cheese!"
-}else if (ham === 0 && cheese === 0) {
-report = "Nothing sold!"
-}else {
-    report = "We had some sales of items"
+if (x===3){
+console.log("X IS EQUAL TO THREE!");
 }
-
-console.log(report);
-
+    x += 5;
+    i += 1;
+}
 
 
 
