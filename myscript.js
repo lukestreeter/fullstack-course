bb6c55a0e84d546538c273b5f9bc8d0c402527b2
marcstreeter <mstreeter@gmail.com>
2021-05-07 13:32:27 -0500

    lecture 63 done

--- myscript.js
+++ myscript.js
@@ -1,33 +1,12 @@
-// var x = 0;
-
-// while (x<5) {
-//     console.log("hello");
-//     x++
-// }
-
-
-
-// for (var i = 0; i < Array.length; i++) {
-//     console.log("hello with for loop");
-// }
-
-
-
-// var num = 1;
-
-// while (num<25) {
-// if (num%2 !== 0){
-//     console.log(num);
-// }
-// num++
-// }
-
-
-for (var i = 0; i < 25; i++) {
-    if (i%2 !== 0) {
-        console.log(i);
-
-    }
-
-
+function timesFive(numInput) {
+    // local scope to the function
+    var result = numInput * 5
+    return result
 }
+
+
+    
+
+    
+
+    
