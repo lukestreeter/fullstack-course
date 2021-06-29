1682a341a84f7461a5d12a4e20da0aa4f77466c9
marcstreeter <mstreeter@gmail.com>
2021-06-29 13:38:03 -0500

    lecture 112 done

--- notes.py
+++ notes.py
@@ -1,5 +1,15 @@
 import re
 
+def multi_re_find(pattern,phrase):
 
-  re.findal 
-   
+   for pat in patterns:
+      print("Searching for pattern {}".format(pat))
+      print(re.findall(pat,phrase))
+      print('/n')
+
+
+test_phrase = 'This is a string with numbers 12312 and a symbol #hashtag'
+  
+  test_patterns = [r'\W+']
+
+  multi_re_find(test_patterns,test_phrase)
