457b0e6daac83cdf1d16cfabfa5a14cee11e6b56
marcstreeter <mstreeter@gmail.com>
2021-07-05 14:32:44 -0500

    lecture 126 done

--- urls.py
+++ urls.py
@@ -1,11 +1,9 @@
-
-from django.conf.urls import url
-from django.contrib import admin
-from django.conf.urls import include
+from django.conf.urls import url,include
+from first_app import views
 from first_app import views
 
 urlpatterns = [
-    url(r'^$',views.help,name='index'),
-    url(r'^help/',include('appTwo.urls')),
+    url(r'^$',views.index,name=index)
     url(r'^admin/', admin.site.urls),
+    url(r'first_app/',include('first_app.urls'))
 ]
--- views.py
+++ views.py
@@ -1,11 +1,6 @@
 from django.shorcuts import render
-from django.http import HttpResponse
+
 # Create your views here.
-
 def index(request):
-    return HttpResponse("<em>My Second Project</em>")
-
-
-def help(request):
-helpdict = {'help_insert':'HELP PAGE'}
-return render(request, 'first_app/index.html',context=helpdict)
+    my_dict = {'insert_content':"HELLO IM FROM FIRSTAPP!"}
+    return render(request, 'first_app/index.html',context=my_dict)
