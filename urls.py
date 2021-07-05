bcae2bb8ccd68404d8e0600fc5d7e8a70bc3fbc5
marcstreeter <mstreeter@gmail.com>
2021-07-05 12:55:33 -0500

    lecture 124 done

--- urls.py
+++ urls.py
@@ -1,10 +1,11 @@
-urlpatterns = [
-    url(r'^$',views.index,name='index'),
-    url(r'^first_app/',include('first.app.urls')),
-    url(r'^admin/', admin.site.urls),
-]
 
 from django.conf.urls import url
 from django.contrib import admin
 from django.conf.urls import include
 from first_app import views
+
+urlpatterns = [
+    url(r'^$',views.help,name='index'),
+    url(r'^help/',include('appTwo.urls')),
+    url(r'^admin/', admin.site.urls),
+]
